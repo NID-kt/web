7e4cd981e0031106dcf3e1389f34c0ec6ab165a4
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-03 23:41:25 +0900

    Add audit logging for user actions

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import PostgresAdapter from '@auth/pg-adapter';
 import NextAuth from 'next-auth';
-import type { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig, User } from 'next-auth';
 import Discord from 'next-auth/providers/discord';
 import GitHub from 'next-auth/providers/github';
 import { Pool } from 'pg';
@@ -13,8 +13,55 @@ const pool = new Pool({
   ssl: true,
 });
 
+const codeBlock = '```';
+
+const sendAuditLog = (method: string, message: object, user: User) => {
+  const formData = new FormData();
+  const payload = JSON.stringify({
+    content: `${codeBlock}json
+    ${JSON.stringify({ method: method, ...user }, null, 2)}
+    ${codeBlock}`,
+    username: user.name,
+    avatar_url: user.image,
+    flags: 4096,
+  });
+
+  formData.append('payload_json', payload);
+
+  const messageBlob = new Blob(
+    [JSON.stringify({ method: method, ...message }, null, 2)],
+    { type: 'application/json' },
+  );
+
+  formData.append('file[0]', messageBlob, 'message.json');
+
+  // biome-ignore lint:noNonNullAssertion - We know this is defined
+  return fetch(process.env.AUDIT_LOG_WEBHOOK!, {
+    method: 'POST',
+    body: formData,
+  });
+};
+
 export const config: NextAuthConfig = {
   adapter: PostgresAdapter(pool),
+  events: {
+    signIn: async (params) => {
+      // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
+      params.account = undefined!;
+      await sendAuditLog('signIn', params, params.user);
+    },
+    createUser: async (params) => {
+      await sendAuditLog('createUser', params, params.user);
+    },
+    updateUser: async (params) => {
+      await sendAuditLog('updateUser', params, params.user);
+    },
+    linkAccount: async (params) => {
+      // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
+      params.account = undefined!;
+      await sendAuditLog('linkAccount', params, params.user);
+    },
+  },
   providers: [
     Discord({
       authorization:
