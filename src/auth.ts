a984e0c217edd2e2bf5a7abd6ed1db5f93926b29
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-03 14:24:10 +0900

    Add authentication support using NextAuth and PostgreSQL adapter

---
+++ src/auth.ts
@@ -0,0 +1,26 @@
+import PostgresAdapter from '@auth/pg-adapter';
+import NextAuth from 'next-auth';
+import type { NextAuthConfig } from 'next-auth';
+import Discord from 'next-auth/providers/discord';
+import GitHub from 'next-auth/providers/github';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+});
+
+export const config: NextAuthConfig = {
+  adapter: PostgresAdapter(pool),
+  providers: [
+    Discord({
+      authorization:
+        'https://discord.com/oauth2/authorize?scope=identify+guilds',
+    }),
+    GitHub,
+  ],
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
