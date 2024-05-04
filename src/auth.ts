e2eb2a897d1b3eee57879dc312db0e2d97f1b639
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-04 23:38:55 +0900

    Refactor sendAuditLog function to properly format JSON payload

--- src/auth.ts
+++ src/auth.ts
@@ -17,22 +17,20 @@ const codeBlock = '```';
 
 const sendAuditLog = (method: string, message: object, user: User) => {
   const formData = new FormData();
+
+  const content = JSON.stringify({ method: method, ...user }, null, 2);
   const payload = JSON.stringify({
-    content: `${codeBlock}json
-    ${JSON.stringify({ method: method, ...user }, null, 2)}
-    ${codeBlock}`,
+    content: `${codeBlock}json\n${content}\n${codeBlock}`,
     username: user.name,
     avatar_url: user.image,
     flags: 4096,
   });
-
   formData.append('payload_json', payload);
 
   const messageBlob = new Blob(
     [JSON.stringify({ method: method, ...message }, null, 2)],
     { type: 'application/json' },
   );
-
   formData.append('file[0]', messageBlob, 'message.json');
 
   // biome-ignore lint:noNonNullAssertion - We know this is defined
