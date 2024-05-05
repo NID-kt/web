82d80f0242e54d01cf499f28e42169fc77eb8b60
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-05 14:58:29 +0900

    Add new properties to User interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,5 +3,7 @@ import type { User } from 'next-auth';
 declare module 'next-auth' {
   interface User {
     isJoinedGuild: boolean;
+    isJoinedOrganization?: boolean;
+    githubUserID?: number;
   }
 }
