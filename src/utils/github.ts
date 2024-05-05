e19e41c9a62f1c499ee0f89ad1a213fae32bd120
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-05 14:58:16 +0900

    Add createOrganizationInvitation function to github.ts

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -18,3 +18,20 @@ export const isJoinedOrganization = async (
   );
   return response.status === 204;
 };
+
+export const createOrganizationInvitation = async (
+  accessToken: string,
+  userID: number,
+) => {
+  const response = await fetch(
+    'https://api.github.com/orgs/NID-roid/invitations',
+    {
+      method: 'POST',
+      headers: createGitHubApiHeaders(accessToken),
+      body: JSON.stringify({
+        invitee_id: userID,
+      }),
+    },
+  );
+  return response.status === 201;
+};
