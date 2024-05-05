854b88b271a8cd37b4a45030f6dbdd13ac1fe1c5
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-05 12:50:32 +0900

    Add createGitHubApiHeaders function to simplify API headers

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,3 +1,11 @@
+const createGitHubApiHeaders = (accessToken: string) => {
+  return {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: `Bearer ${accessToken}`,
+    'X-GitHub-Api-Version': '2022-11-28',
+  };
+};
+
 export const isJoinedOrganization = async (
   accessToken: string,
   username: string,
@@ -5,11 +13,7 @@ export const isJoinedOrganization = async (
   const response = await fetch(
     `https://api.github.com/orgs/NID-roid/members${username}`,
     {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `Bearer ${accessToken}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
+      headers: createGitHubApiHeaders(accessToken),
     },
   );
   return response.status === 204;
