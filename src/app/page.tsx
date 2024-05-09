285252dbee6da7fad1789a625d298f49a5945cb2
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-09 20:08:44 +0900

    Add sign-in buttons and organization invitation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,47 @@
 import { auth, signIn, signOut } from '@/auth';
+import { createOrganizationInvitation } from '@/utils/github';
+
+const ButtonInForm = ({
+  action,
+  text,
+}: {
+  action: () => void;
+  text: string;
+}) => (
+  <form action={action}>
+    <button
+      type='submit'
+      className='px-8 py-4 mt-8 text-lg font-semibold border border-gray-300 rounded-lg transition-colors hover:border-gray-400'
+    >
+      {text}
+    </button>
+  </form>
+);
+
+const SignInButton = ({
+  service,
+  text,
+}: {
+  service: 'discord' | 'github';
+  text: string;
+}) => (
+  <ButtonInForm
+    action={async () => {
+      'use server';
+      await signIn(service);
+    }}
+    text={text}
+  />
+);
+
+const DiscordLink = () => (
+  <a
+    href='https://discord.gg/nid-kt'
+    className='px-8 py-4 mt-8 text-lg font-semibold border border-gray-300 rounded-lg transition-colors hover:border-gray-400'
+  >
+    Join NID.kt Discord Server!
+  </a>
+);
 
 export default async function Home() {
   const session = await auth();
@@ -6,39 +49,43 @@ export default async function Home() {
   if (!session) {
     return (
       <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-        <form
-          action={async () => {
-            'use server';
-            await signIn('discord');
-          }}
-        >
-          <button
-            type='submit'
-            className='px-8 py-4 text-lg font-semibold border border-gray-300 rounded-lg transition-colors hover:border-gray-400'
-          >
-            Sign in with Discord
-          </button>
-        </form>
+        <SignInButton service='discord' text='Sign in with Discord' />
       </main>
     );
   }
 
+  const { isJoinedGuild, githubUserID, isJoinedOrganization, name } =
+    session.user || {};
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
-      <p className='text-2xl font-semibold'>Welcome {session.user?.name}</p>
-      <form
+      <p className='text-2xl font-semibold'>Welcome {name}</p>
+      {!isJoinedGuild ? (
+        <DiscordLink />
+      ) : !githubUserID ? (
+        <SignInButton service='github' text='Sign in with GitHub' />
+      ) : !isJoinedOrganization ? (
+        <ButtonInForm
+          action={async () => {
+            'use server';
+            await createOrganizationInvitation(
+              // biome-ignore lint:noNonNullAssertion - We know this is defined
+              process.env.GITHUB_ACCESS_TOKEN!,
+              githubUserID,
+            );
+          }}
+          text='Join NID-roid Organization!'
+        />
+      ) : (
+        <></>
+      )}
+      <ButtonInForm
         action={async () => {
           'use server';
           await signOut();
         }}
-      >
-        <button
-          type='submit'
-          className='px-8 py-4 mt-8 text-lg font-semibold border border-gray-300 rounded-lg transition-colors hover:border-gray-400'
-        >
-          Sign out
-        </button>
-      </form>
+        text='Sign out'
+      />
     </main>
   );
 }
