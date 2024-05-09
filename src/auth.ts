0e1795053d8a492b100bafee5a2fa21aee5472d6
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-09 13:23:47 +0900

    Update import paths in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -5,8 +5,8 @@ import Discord from 'next-auth/providers/discord';
 import GitHub from 'next-auth/providers/github';
 import { Pool } from 'pg';
 
-import { isJoinedGuild } from './utils/discord';
-import { isJoinedOrganization } from './utils/github';
+import { isJoinedGuild } from '@/utils/discord';
+import { isJoinedOrganization } from '@/utils/github';
 
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
