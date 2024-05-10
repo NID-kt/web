f7e2b3012f08f09f0381af5d99e4997292d9f39f
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-11 02:40:24 +0900

    Add functions for creating and sending direct messages in Discord

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -10,3 +10,49 @@ export const isJoinedGuild = async (accessToken: string) => {
   const json = (await response.json()) as { id: string }[];
   return json.some((guild) => guild.id === process.env.DISCORD_GUILD_ID);
 };
+
+export const createDirectMessageChannel = async (userID: string) => {
+  const response = await fetch(
+    'https://discord.com/api/v10/users/@me/channels',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        recipient_id: userID,
+      }),
+    },
+  );
+
+  const json = await response.json();
+  return json.id;
+};
+
+export const sendMessage = ({
+  channelID,
+  message,
+}: { channelID: string; message: string }) => {
+  return fetch(`https://discord.com/api/v10/channels/${channelID}/messages`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: message,
+    }),
+  });
+};
+
+export const sendDirectMessage = async ({
+  userID,
+  message,
+}: {
+  userID: string;
+  message: string;
+}) => {
+  const channelID = await createDirectMessageChannel(userID);
+  return await sendMessage({ channelID, message });
+};
