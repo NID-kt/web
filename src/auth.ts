abdf3bfd12517021bc88f39b7d713ba65c04b721
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-15 18:42:27 +0900

    Add NextRequest type to config function

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import type { NextAuthConfig, User } from 'next-auth';
 import Discord, { type DiscordProfile } from 'next-auth/providers/discord';
 import GitHub from 'next-auth/providers/github';
+import type { NextRequest } from 'next/server';
 import { Pool } from 'pg';
 
 import PostgresAdapter from '@/db/adapter-pg';
@@ -45,89 +46,91 @@ const sendAuditLog = (method: string, message: object, user: User) => {
 
 const adapter = PostgresAdapter(pool);
 
-export const config: NextAuthConfig = {
-  adapter: adapter,
-  callbacks: {
-    async signIn({ user, account }) {
-      if (user.discordUserID) {
-        await sendDirectMessage({
-          userID: user.discordUserID,
-          message: `[NID.kt](https://discord.gg/nid-kt) の Web サイトへようこそ！✨🙌🏻\n\`${account?.provider}\` でログインしました ✅`,
-        });
-        return true;
-      }
-      return false;
+export const config = (request: NextRequest | undefined): NextAuthConfig => {
+  return {
+    adapter: adapter,
+    callbacks: {
+      async signIn({ user, account }) {
+        if (user.discordUserID) {
+          await sendDirectMessage({
+            userID: user.discordUserID,
+            message: `[NID.kt](https://discord.gg/nid-kt) の Web サイトへようこそ！✨🙌🏻\n\`${account?.provider}\` でログインしました ✅`,
+          });
+          return true;
+        }
+        return false;
+      },
     },
-  },
-  events: {
-    signIn: async (params) => {
-      // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
-      params.account = undefined!;
-      await sendAuditLog('signIn', params, params.user);
-    },
-    createUser: async (params) => {
-      await sendAuditLog('createUser', params, params.user);
-    },
-    updateUser: async (params) => {
-      await sendAuditLog('updateUser', params, params.user);
-    },
-    linkAccount: async (params) => {
-      const user = params.user;
-      if ('emailVerified' in user && adapter.updateUser) {
-        const {
-          isJoinedGuild,
-          isJoinedOrganization,
-          githubUserID,
-          githubUserName,
-        } = params.profile;
+    events: {
+      signIn: async (params) => {
+        // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
+        params.account = undefined!;
+        await sendAuditLog('signIn', params, params.user);
+      },
+      createUser: async (params) => {
+        await sendAuditLog('createUser', params, params.user);
+      },
+      updateUser: async (params) => {
+        await sendAuditLog('updateUser', params, params.user);
+      },
+      linkAccount: async (params) => {
+        const user = params.user;
+        if ('emailVerified' in user && adapter.updateUser) {
+          const {
+            isJoinedGuild,
+            isJoinedOrganization,
+            githubUserID,
+            githubUserName,
+          } = params.profile;
 
-        await adapter.updateUser({
-          ...user,
-          isJoinedGuild: user.isJoinedGuild ?? isJoinedGuild,
-          isJoinedOrganization:
-            user.isJoinedOrganization ?? isJoinedOrganization,
-          githubUserID: user.githubUserID ?? githubUserID,
-          githubUserName: user.githubUserName ?? githubUserName,
-        });
-      }
-      // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
-      params.account = undefined!;
-      await sendAuditLog('linkAccount', params, user);
+          await adapter.updateUser({
+            ...user,
+            isJoinedGuild: user.isJoinedGuild ?? isJoinedGuild,
+            isJoinedOrganization:
+              user.isJoinedOrganization ?? isJoinedOrganization,
+            githubUserID: user.githubUserID ?? githubUserID,
+            githubUserName: user.githubUserName ?? githubUserName,
+          });
+        }
+        // biome-ignore lint:noNonNullAssertion - To avoid sending sensitive data
+        params.account = undefined!;
+        await sendAuditLog('linkAccount', params, user);
+      },
     },
-  },
-  providers: [
-    Discord<DiscordProfile>({
-      authorization:
-        'https://discord.com/oauth2/authorize?scope=identify+guilds',
-      profile: async (profile, token) => {
-        const user = {
-          ...(await Discord({}).profile?.(profile, token)),
-          discordUserID: profile.id,
-        };
-        if (token?.access_token) {
-          user.isJoinedGuild = await isJoinedGuild(token.access_token);
-        }
-        return user;
-      },
-    }),
-    GitHub({
-      profile: async (profile, token) => {
-        const user = {
-          ...(await GitHub({}).profile?.(profile, token)),
-          githubUserID: profile.id,
-          githubUserName: profile.login,
-        };
-        if (user.githubUserName) {
-          user.isJoinedOrganization = await isJoinedOrganization(
-            // biome-ignore lint:noNonNullAssertion - We know this is defined
-            process.env.GITHUB_ACCESS_TOKEN!,
-            user.githubUserName,
-          );
-        }
-        return user;
-      },
-    }),
-  ],
+    providers: [
+      Discord<DiscordProfile>({
+        authorization:
+          'https://discord.com/oauth2/authorize?scope=identify+guilds',
+        profile: async (profile, token) => {
+          const user = {
+            ...(await Discord({}).profile?.(profile, token)),
+            discordUserID: profile.id,
+          };
+          if (token?.access_token) {
+            user.isJoinedGuild = await isJoinedGuild(token.access_token);
+          }
+          return user;
+        },
+      }),
+      GitHub({
+        profile: async (profile, token) => {
+          const user = {
+            ...(await GitHub({}).profile?.(profile, token)),
+            githubUserID: profile.id,
+            githubUserName: profile.login,
+          };
+          if (user.githubUserName) {
+            user.isJoinedOrganization = await isJoinedOrganization(
+              // biome-ignore lint:noNonNullAssertion - We know this is defined
+              process.env.GITHUB_ACCESS_TOKEN!,
+              user.githubUserName,
+            );
+          }
+          return user;
+        },
+      }),
+    ],
+  };
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
