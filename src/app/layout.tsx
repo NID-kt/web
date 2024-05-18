b2b7da666b07366bbb930333ef591bc60d80ad96
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-05-18 23:30:36 +0900

    Add metadataBase URL to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.nidkt.org/'),
   title: 'NID.kt',
   description: '学生・社会人混合の Android エンジニアコミュニティです',
 };
