2ce59b7a2cd12771fe99903b64380cd75b95c395
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-07-29 19:25:28 +0900

    chore: Update redirects in next.config.mjs and vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: () => {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'discord.nidkt.org' }],
+        destination: 'https://discord.gg/nid-kt',
+        permanent: true,
+      },
+      {
+        source: '/invite',
+        has: [{ type: 'host', value: 'discord.nidkt.org' }],
+        destination: 'https://discord.gg/nid-kt',
+        permanent: true,
+      },
+      {
+        source: '/event/lt-vol-3',
+        has: [{ type: 'host', value: 'discord.nidkt.org' }],
+        destination: 'https://discord.gg/nid-kt?event=1260990331193917541',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
